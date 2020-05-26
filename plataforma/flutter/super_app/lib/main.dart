89bd87ee4cdecf17a7ea6b764139482328ebbb7f
Alex Matos <amatosolivo@gmail.com>
2020-05-25 22:09:29 -0400

    Proyecto recien creado en Flutter

---
+++ plataforma/flutter/super_app/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Super());
+}
+
+class Super extends StatefulWidget {
+  @override
+  _SuperState createState() => _SuperState();
+}
+
+class _SuperState extends State<Super> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

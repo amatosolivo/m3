85c83e0ad6a09492555cfb95fae37ffefd337f5c
Alex Matos <amatosolivo@gmail.com>
2020-06-02 02:37:53 -0400

    Proyecto para aclarar DDD

---
+++ plataforma/flutter/super_app/lib/dominio/autenticacion/fallo_autenticacion.freezed.dart
@@ -0,0 +1,76 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named
+
+part of 'fallo_autenticacion.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+class _$FalloAutentiacionTearOff {
+  const _$FalloAutentiacionTearOff();
+
+  CanceladoPorUsuario canceladoPorUsuario() {
+    return const CanceladoPorUsuario();
+  }
+}
+
+// ignore: unused_element
+const $FalloAutentiacion = _$FalloAutentiacionTearOff();
+
+mixin _$FalloAutentiacion {}
+
+abstract class $FalloAutentiacionCopyWith<$Res> {
+  factory $FalloAutentiacionCopyWith(
+          FalloAutentiacion value, $Res Function(FalloAutentiacion) then) =
+      _$FalloAutentiacionCopyWithImpl<$Res>;
+}
+
+class _$FalloAutentiacionCopyWithImpl<$Res>
+    implements $FalloAutentiacionCopyWith<$Res> {
+  _$FalloAutentiacionCopyWithImpl(this._value, this._then);
+
+  final FalloAutentiacion _value;
+  // ignore: unused_field
+  final $Res Function(FalloAutentiacion) _then;
+}
+
+abstract class $CanceladoPorUsuarioCopyWith<$Res> {
+  factory $CanceladoPorUsuarioCopyWith(
+          CanceladoPorUsuario value, $Res Function(CanceladoPorUsuario) then) =
+      _$CanceladoPorUsuarioCopyWithImpl<$Res>;
+}
+
+class _$CanceladoPorUsuarioCopyWithImpl<$Res>
+    extends _$FalloAutentiacionCopyWithImpl<$Res>
+    implements $CanceladoPorUsuarioCopyWith<$Res> {
+  _$CanceladoPorUsuarioCopyWithImpl(
+      CanceladoPorUsuario _value, $Res Function(CanceladoPorUsuario) _then)
+      : super(_value, (v) => _then(v as CanceladoPorUsuario));
+
+  @override
+  CanceladoPorUsuario get _value => super._value as CanceladoPorUsuario;
+}
+
+class _$CanceladoPorUsuario implements CanceladoPorUsuario {
+  const _$CanceladoPorUsuario();
+
+  @override
+  String toString() {
+    return 'FalloAutentiacion.canceladoPorUsuario()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) || (other is CanceladoPorUsuario);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+}
+
+abstract class CanceladoPorUsuario implements FalloAutentiacion {
+  const factory CanceladoPorUsuario() = _$CanceladoPorUsuario;
+}

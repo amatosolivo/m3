b549fe5891c3f141fdf582360e15779ec8f00c35
Alex Matos <amatosolivo@gmail.com>
2020-06-04 16:02:23 -0400

    Pagina Home agregada

--- plataforma/frontend/super_app/lib/pantallas/home.dart
+++ plataforma/frontend/super_app/lib/pantallas/home.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
   static String nombreRuta = 'home';
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('SuperApp'),
+          backgroundColor: Colors.grey,
+        ),
+      ),
+    );
   }
 }

6a8ac963866b5599b05b96d558d8fd9d354d9946
alucard720 <ing.santana36@gmail.com>
2021-07-08 07:49:36 -0400

    actualizacion win11 1.0

--- plataforma/frontend/super_app/android/app/src/main/kotlin/com/malcubo/superapp/MainActivity.kt
+++ plataforma/frontend/super_app/android/app/src/main/kotlin/com/malcubo/superapp/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.malcubo.superapp
 
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
-import androidx.annotation.NonNull;
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
+class MainActivity: FlutterFragmentActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
 }
